b50108dc06dc3d6c77a8798b8518041c5248a797
Kundan <reachkundankumar@gmail.com>
2025-01-28 13:24:54 +0530

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Yashwant Yadav</strong>
+                <strong className="main-name">Kundan Soni</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
