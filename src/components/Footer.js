bedf5e4b4456429dcfef428bd47e1c829b6d82aa
Kundan <reachkundankumar@gmail.com>
2025-01-28 12:49:04 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Yashwant Yadav</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} YY</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/Yashwantyadav1"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/yashwant-yadav-b05b38228?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/_yadav_yashwant?igsh=MXJhNDRocmRoODZtZg=="
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import music from "../../Assets/Projects/music .jpeg";
+import shoping from "../../Assets/Projects/shoping.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={music}
+              isBlog={false}
+              title="Music"
+              description=" Web App offers a seamless music streaming experience using HTML, CSS, and JavaScript for the front-end, and MongoDB for the back-end.
+              Users can stream music, create and manage playlists, authenticate securely, and receivepersonalized music recommendations, providing an engaging, responsive, and interactiveweb-based music platform.."
+              ghLink="https://github.com/Yashwantyadav1/Music"
+              demoLink="Avaliable Soon"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={shoping}
+              isBlog={false}
+              title="Shoping "
+              description=" e-commerce website using HTML, CSS, and JavaScript. Deliver a seamless shopping experience, intuitive navigation, and interactive product displays. Implement secure checkout, and cart functionality."
+              ghLink="https://github.com/Yashwantyadav1/project"
+              demoLink="https://yashwantyadav1.github.io/project/"
+            />
+          </Col>
+
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
